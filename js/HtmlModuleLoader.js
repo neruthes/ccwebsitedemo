174da4a9ff814f5baaef8dd7900d4f0065ec5346
Neruthes <i@neruthes.xyz>
2021-03-18 05:22:15 +0000

    category-major: Added entry cell theme feature

--- js/HtmlModuleLoader.js
+++ js/HtmlModuleLoader.js
@@ -1,19 +1,31 @@
 const HtmlModuleLoader = {};
+const HtmlModuleCache = {};
 
 HtmlModuleLoader.load = function (modName, instanceIndex, options) {
-    document.write(`<div data-HtmlModuleLoader-placeholder="${modName}" data-HtmlModuleLoader-placeholderIndex="${instanceIndex}"></div>`);
+    let prng = Math.random().toString().slice(2);
+    document.write(`<div data-HtmlModuleLoader-placeholder="${modName}" data-HtmlModuleLoader-placeholderIndex="${instanceIndex}" data-prng="${prng}"></div>`);
+    if (HtmlModuleCache[modName]) {
+        HtmlModuleLoader.put(modName, options, instanceIndex, prng);
+        return 0;
+    };
     let xhr = new XMLHttpRequest();
     xhr.open('GET', `/html-modules/${modName}.html`);
     xhr.send();
     xhr.onload = function (e) {
         // console.log(`[data-HtmlModuleLoader-placeholder="${modName}"][data-HtmlModuleLoader-placeholderIndex="${instanceIndex}"]`);
         if (e.target.status === 200 || e.target.status === 304) {
-            console.log(document.querySelector(`[data-HtmlModuleLoader-placeholder="${modName}"][data-HtmlModuleLoader-placeholderIndex="${instanceIndex}"]`));
-            document.querySelector(`[data-HtmlModuleLoader-placeholder="${modName}"][data-HtmlModuleLoader-placeholderIndex="${instanceIndex}"]`).outerHTML = HtmlModuleLoader.render(e.target.responseText, options);
+            HtmlModuleCache[modName] = e.target.responseText;
+            // console.log(document.querySelector(`[data-HtmlModuleLoader-placeholder="${modName}"][data-HtmlModuleLoader-placeholderIndex="${instanceIndex}"]`));
+            HtmlModuleLoader.put(modName, options, instanceIndex, prng);
         } else {
             console.log(`HtmlModuleLoader: Error: Cannot load ${modName} (${e.target.status})`);
         };
     };
+};
+
+
+HtmlModuleLoader.put = function (modName, options, instanceIndex, prng) {
+    document.querySelector(`[data-HtmlModuleLoader-placeholder="${modName}"][data-HtmlModuleLoader-placeholderIndex="${instanceIndex}"][data-prng="${prng}"]`).outerHTML = HtmlModuleLoader.render(HtmlModuleCache[modName], options);
 };
 
 HtmlModuleLoader.render = function (inputTmpl, options) {
@@ -21,13 +33,13 @@ HtmlModuleLoader.render = function (inputTmpl, options) {
     let tmpl = inputTmpl;
     for (var i = 0; i < 255; i++) {
         let matchResult = tmpl.match(myMatchCriteria);
-        console.log(`Matched!`, matchResult);
+        // console.log(`Matched!`, matchResult);
         if (matchResult) {
             tmpl = tmpl.replace(matchResult[0], options[matchResult[1]]);
         } else {
             return tmpl;
         };
     };
-    console.log(tmpl);
+    // console.log(tmpl);
     return tmpl;
 };
